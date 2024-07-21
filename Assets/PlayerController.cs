c09d49835aa90b6b710f6b7a7b5b923adcca5a64
KINGCON3 <92693654+KINGCON3@users.noreply.github.com>
2024-07-21 16:02:46 +0100

    Movement and animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,9 +14,12 @@ public class PlayerController : MonoBehaviour
     private InputAction move;
     private InputAction fire;
 
+    private Animator animator;
+
     private void Awake()
     {
         playerControls = new PlayerInputActions();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -43,7 +47,56 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
+        //x,y,z
+        Vector3 currentVector = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
+
+
+        Quaternion newRotation = rb.rotation; // Start with the current rotation
+
+        if (currentVector.x != 0 || currentVector.z != 0)
+        {
+            if (currentVector.x > 0 && currentVector.z > 0)
+            {
+                newRotation = Quaternion.Euler(0, 45, 0);  // Face top-right
+            }
+            else if (currentVector.x > 0 && currentVector.z < 0)
+            {
+                newRotation = Quaternion.Euler(0, 135, 0); // Face bottom-right
+            }
+            else if (currentVector.x < 0 && currentVector.z > 0)
+            {
+                newRotation = Quaternion.Euler(0, 315, 0); // Face top-left
+            }
+            else if (currentVector.x < 0 && currentVector.z < 0)
+            {
+                newRotation = Quaternion.Euler(0, 225, 0); // Face bottom-left
+            }
+            else if (currentVector.x > 0)
+            {
+                newRotation = Quaternion.Euler(0, 90, 0);  // Face right
+            }
+            else if (currentVector.x < 0)
+            {
+                newRotation = Quaternion.Euler(0, 270, 0); // Face left
+            }
+            else if (currentVector.z > 0)
+            {
+                newRotation = Quaternion.Euler(0, 0, 0);   // Face forward
+            }
+            else if (currentVector.z < 0)
+            {
+                newRotation = Quaternion.Euler(0, 180, 0); // Face backward
+            }
+
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        rb.rotation = newRotation;
+        rb.velocity = currentVector;
     }
 
     private void Fire(InputAction.CallbackContext context)
