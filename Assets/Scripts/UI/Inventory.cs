658e32d6c25800cd483c1ad556981931c8111887
KINGCON3 <92693654+KINGCON3@users.noreply.github.com>
2024-07-21 22:02:17 +0100

    Inventory part 2

---
+++ Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeReference] public List<ItemSlotInfo> items = new List<ItemSlotInfo>();
+
+    [Space]
+    [Header("inventory Menu Components")]
+    public GameObject inventoryMenu;
+    public GameObject itemPanel;
+    public GameObject itemPanelGrid;
+
+    private List<ItemPanel> existingPanels = new List<ItemPanel>();
+
+    [Space]
+    public int inventorySize = 44;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            items.Add(new ItemSlotInfo(null,0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+               if (inventoryMenu.activeSelf)
+            {
+                inventoryMenu.SetActive(false);
+                //Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                inventoryMenu.SetActive(true);
+                //Cursor.lockState = CursorLockMode.Confined;
+                RefreshInventory();
+            }
+        }
+    }
+
+    public void RefreshInventory()
+    {
+        existingPanels = itemPanelGrid.GetComponentsInChildren<ItemPanel>().ToList();
+        //Create Panels if needed
+        if (existingPanels.Count < inventorySize)
+        {
+            int amountToCreate = inventorySize - existingPanels.Count;
+            for (int i = 0; i < amountToCreate; i++)
+            {
+                GameObject newPanel = Instantiate(itemPanel, itemPanelGrid.transform);
+                existingPanels.Add(newPanel.GetComponent<ItemPanel>());
+            }
+        }
+
+        int index = 0;
+        foreach (ItemSlotInfo i in items)
+        {
+            //name List Elements
+            i.name = "" + (index + 1);
+            if (i.item != null) i.name += ": " + i.item.GiveName();
+            else i.name += ": -";
+
+            //Update Panels
+            ItemPanel panel = existingPanels[index];
+            if (panel != null)
+            {
+                panel.name = i.name + " Panel";
+                panel.inventory = this;
+                panel.itemSlot = i;
+                //panel.slotInfo = i;
+                if (i.item != null)
+                {
+                    panel.itemImage.gameObject.SetActive(true);
+                    panel.itemImage.sprite = i.item.GiveItemImage();
+                    panel.stacksText.gameObject.SetActive(true);
+                    panel.stacksText.text = "" + i.stacks;
+                }
+                else
+                {
+                    panel.itemImage.gameObject.SetActive(false);
+                    panel.stacksText.gameObject.SetActive(false);
+                }
+            }
+            index++;
+        }
+    }
+
+    public int AddItem(Item item, int amount)
+    {
+        //Check for open spaces in existing slots
+        foreach(ItemSlotInfo i in items)
+        {
+            if (i.item != null)
+            {
+                if (i.item.GiveName() == item.GiveName())
+                {
+                    if (amount > i.item.MaxStacks() - i.stacks)
+                    {
+                        amount -= i.item.MaxStacks() - i.stacks;
+                        i.stacks = i.item.MaxStacks();
+                    }
+                    else
+                    {
+                        i.stacks += amount;
+                        if (inventoryMenu.activeSelf) RefreshInventory();
+                        return 0;
+                    }
+                }
+            }
+        }
+        //Fill empty slots with leftover items
+        foreach(ItemSlotInfo i in items)
+        {
+            if (i.item != null)
+            {
+                if (amount > item.MaxStacks())
+                {
+                    i.item = item;
+                    i.stacks = item.MaxStacks();
+                    amount -= item.MaxStacks();
+                }
+                else
+                {
+                    i.item = item;
+                    i.stacks = amount;
+                    if (inventoryMenu.activeSelf ) RefreshInventory();
+                    return 0;
+                }
+            }
+        }
+        //No space in Inventory, return remainder items
+        Debug.Log("No space in Inventory for: " + item.GiveName());
+        if (inventoryMenu.activeSelf) RefreshInventory();
+        return amount;
+    }
+
+    public void ClearSlot(ItemSlotInfo slot)
+    {
+        slot.item = null;
+        slot.stacks = 0;
+    }
+}
---
+++ Assets/Scripts/UI/ItemPanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemPanel : MonoBehaviour
+{
+    public Inventory inventory;
+    //public ItemSlotInfo slotInfo;
+    public ItemSlotInfo itemSlot;
+    public Image itemImage;
+    public TextMeshProUGUI stacksText;
+}
